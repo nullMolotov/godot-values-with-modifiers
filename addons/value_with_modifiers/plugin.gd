5c16130db8e14662bc6915be42930f12725c62ff
nullMolotov <119440040+nullMolotov@users.noreply.github.com>
2023-07-23 23:26:51 -0600

    Update plugin.gd

--- addons/value_with_modifiers/plugin.gd
+++ addons/value_with_modifiers/plugin.gd
@@ -1,12 +1,15 @@
 @tool
 extends EditorPlugin
 
+var placeholder_texture := PlaceholderTexture2D.new()
 
 func _enter_tree():
-	# Initialization of the plugin goes here.
-	pass
+	add_custom_type("FloatWithModifiers", "RefCounted", FloatWithModifiers, placeholder_texture)
+	add_custom_type("IntWithModifiers", "RefCounted", IntWithModifiers, placeholder_texture)
+	add_custom_type("ConditionEvaluator", "RefCounted", ConditionEvaluator, placeholder_texture)
 
 
 func _exit_tree():
-	# Clean-up of the plugin goes here.
-	pass
+	remove_custom_type("FloatWithModifiers")
+	remove_custom_type("IntWithModifiers")
+	remove_custom_type("ConditionEvaluator")
